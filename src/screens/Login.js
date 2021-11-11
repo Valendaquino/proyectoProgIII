016f6df1f8da87d43136f3134da41b48867d50eb
paulinaruiz <pruiz@udesa.edu.ar>
2021-11-11 17:29:27 -0300

    arranco login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -19,25 +19,42 @@ class Login extends Component{
     render(){
         return (
             <View>
-            <Text>hola</Text>
-             
+            <Text>{this.props.error}</Text>
+             <TextInput
+             onChangeText={(text)=> this.setState({email:text})}
+             placeholder="email"
+             keyboardType="email-addres"
+             />
+             <TextInput
+             onChangeText={(text)=> this.setState({password:text})}
+             placeholder="password"
+             keyboardType="email-addres"
+             secureTextEntry={true} />
+             <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.props.login(this.state.email, this.state.password)}
+              >
+                <Text style={styles.textButton}>Ingresar</Text>
+              </TouchableOpacity>
+
             </View>
-          );
-        }
-      }
-      const styles = StyleSheet.create({
-        button: {
-          backgroundColor: "#28a745",
-          paddingHorizontal: 10,
-          paddingVertical: 6,
-          textAlign: "center",
-          borderRadius: 4,
-          borderWidth: 1,
-          borderStyle: "solid",
-          borderColor: "#28a745",
-        },
-        textButton: {
-          color: "#fff",
-        },
-      });
-export default Login;
+              );
+            }
+          }
+          const styles = StyleSheet.create({
+            button: {
+              backgroundColor: "#28a745",
+              paddingHorizontal: 10,
+              paddingVertical: 6,
+              textAlign: "center",
+              borderRadius: 4,
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderColor: "#28a745",
+            },
+            textButton: {
+              color: "#fff",
+            },
+          });
+    export default Login;
+         
